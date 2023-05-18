3fa13877ca15fc170d3c4c739e8835cb3df9b897
danny.wong@databricks.com <danny.wong@databricks.com>
2023-05-18 13:39:14 +0000

    modified langchain notebook

--- examples/langchain DW.py
+++ examples/langchain DW.py
@@ -15,90 +15,6 @@
 
 # COMMAND ----------
 
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
-
-# COMMAND ----------
-
-default_model = "databricks/dolly-v2-3b"
-
-suggested_models = [
-    "databricks/dolly-v1-6b",
-    "databricks/dolly-v2-3b",
-    "databricks/dolly-v2-7b",
-    "databricks/dolly-v2-12b",
-]
-
-dbutils.widgets.combobox("input_model", default_model, suggested_models, "input_model")
-
-# COMMAND ----------
-
-from training.generate import InstructionTextGenerationPipeline, load_model_tokenizer_for_generate
-
-input_model = dbutils.widgets.get("input_model")
-
-#model, tokenizer = load_model_tokenizer_for_generate(input_model)
-model, tokenizer = load_model_tokenizer_for_generate("/dbfs/dolly_training/dolly__2023-05-18T07:35:00")
-
-# COMMAND ----------
-
-from langchain import PromptTemplate, LLMChain
-from langchain.llms import HuggingFacePipeline
-
-# template for an instrution with no input
-prompt = PromptTemplate(
-    input_variables=["instruction"],
-    template="{instruction}")
-
-# template for an instruction with input
-prompt_with_context = PromptTemplate(
-    input_variables=["instruction", "context"],
-    template="{instruction}\n\nInput:\n{context}")
-
-hf_pipeline = HuggingFacePipeline(
-    pipeline=InstructionTextGenerationPipeline(
-        # Return the full text, because this is what the HuggingFacePipeline expects.
-        model=model, tokenizer=tokenizer, return_full_text=True, task="text-generation"))
-
-llm_chain = LLMChain(llm=hf_pipeline, prompt=prompt)
-llm_context_chain = LLMChain(llm=hf_pipeline, prompt=prompt_with_context)
-
-# COMMAND ----------
-
-# Examples from https://www.databricks.com/blog/2023/03/24/hello-dolly-democratizing-magic-chatgpt-open-models.html
-instructions = [
-    "Explain to me the difference between nuclear fission and fusion.",
-    "Give me a list of 5 science fiction books I should read next.",
-]
-
-# Use the model to generate responses for each of the instructions above.
-for instruction in instructions:
-    response = llm_chain.predict(instruction=instruction)
-    print(f"Instruction: {instruction}\n\n{response}\n\n-----------\n")
-
-# COMMAND ----------
-
-context = (
-    """George Washington (February 22, 1732[b] – December 14, 1799) was an American military officer, statesman, """
-    """and Founding Father who served as the first president of the United States from 1789 to 1797. Appointed by """
-    """the Continental Congress as commander of the Continental Army, Washington led Patriot forces to victory in """
-    """the American Revolutionary War and served as president of the Constitutional Convention of 1787, which """
-    """created and ratified the Constitution of the United States and the American federal government. Washington """
-    """has been called the "Father of his Country" for his manifold leadership in the nation's founding."""
-)
-
-instruction = "When did George Washinton serve as president of the Constitutional Convention?"
-
-response = llm_context_chain.predict(instruction=instruction, context=context)
-print(f"Instruction: {instruction}\n\nContext:\n{context}\n\nResponse:\n{response}\n\n-----------\n")
-
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC ## Text to SQL test
-
-# COMMAND ----------
-
 # MAGIC %pip install -U sqlalchemy sqlalchemy-databricks
 
 # COMMAND ----------
@@ -107,6 +23,90 @@ dbutils.library.restartPython()
 
 # COMMAND ----------
 
+# MAGIC %load_ext autoreload
+# MAGIC %autoreload 2
+
+# COMMAND ----------
+
+# default_model = "databricks/dolly-v2-3b"
+
+# suggested_models = [
+#     "databricks/dolly-v1-6b",
+#     "databricks/dolly-v2-3b",
+#     "databricks/dolly-v2-7b",
+#     "databricks/dolly-v2-12b",
+# ]
+
+# dbutils.widgets.combobox("input_model", default_model, suggested_models, "input_model")
+
+# COMMAND ----------
+
+from training.generate import InstructionTextGenerationPipeline, load_model_tokenizer_for_generate
+
+# input_model = dbutils.widgets.get("input_model")
+
+#model, tokenizer = load_model_tokenizer_for_generate(input_model)
+model, tokenizer = load_model_tokenizer_for_generate("/dbfs/dolly_training/dolly__2023-05-18T07:35:00") #hardcode
+
+# COMMAND ----------
+
+# from langchain import PromptTemplate, LLMChain
+# from langchain.llms import HuggingFacePipeline
+
+# # template for an instrution with no input
+# prompt = PromptTemplate(
+#     input_variables=["instruction"],
+#     template="{instruction}")
+
+# # template for an instruction with input
+# prompt_with_context = PromptTemplate(
+#     input_variables=["instruction", "context"],
+#     template="{instruction}\n\nInput:\n{context}")
+
+# hf_pipeline = HuggingFacePipeline(
+#     pipeline=InstructionTextGenerationPipeline(
+#         # Return the full text, because this is what the HuggingFacePipeline expects.
+#         model=model, tokenizer=tokenizer, return_full_text=True, task="text-generation"))
+
+# llm_chain = LLMChain(llm=hf_pipeline, prompt=prompt)
+# llm_context_chain = LLMChain(llm=hf_pipeline, prompt=prompt_with_context)
+
+# COMMAND ----------
+
+# # Examples from https://www.databricks.com/blog/2023/03/24/hello-dolly-democratizing-magic-chatgpt-open-models.html
+# instructions = [
+#     "Explain to me the difference between nuclear fission and fusion.",
+#     "Give me a list of 5 science fiction books I should read next.",
+# ]
+
+# # Use the model to generate responses for each of the instructions above.
+# for instruction in instructions:
+#     response = llm_chain.predict(instruction=instruction)
+#     print(f"Instruction: {instruction}\n\n{response}\n\n-----------\n")
+
+# COMMAND ----------
+
+# context = (
+#     """George Washington (February 22, 1732[b] – December 14, 1799) was an American military officer, statesman, """
+#     """and Founding Father who served as the first president of the United States from 1789 to 1797. Appointed by """
+#     """the Continental Congress as commander of the Continental Army, Washington led Patriot forces to victory in """
+#     """the American Revolutionary War and served as president of the Constitutional Convention of 1787, which """
+#     """created and ratified the Constitution of the United States and the American federal government. Washington """
+#     """has been called the "Father of his Country" for his manifold leadership in the nation's founding."""
+# )
+
+# instruction = "When did George Washinton serve as president of the Constitutional Convention?"
+
+# response = llm_context_chain.predict(instruction=instruction, context=context)
+# print(f"Instruction: {instruction}\n\nContext:\n{context}\n\nResponse:\n{response}\n\n-----------\n")
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ## Text to SQL test
+
+# COMMAND ----------
+
 from training.generate import InstructionTextGenerationPipeline, load_model_tokenizer_for_generate
 
 input_model = dbutils.widgets.get("input_model")
@@ -119,15 +119,15 @@ model, tokenizer = load_model_tokenizer_for_generate("/dbfs/dolly_training/dolly
 from langchain import PromptTemplate, LLMChain
 from langchain.llms import HuggingFacePipeline
 
-# template for an instrution with no input
-prompt = PromptTemplate(
-    input_variables=["instruction"],
-    template="{instruction}")
+# # template for an instrution with no input
+# prompt = PromptTemplate(
+#     input_variables=["instruction"],
+#     template="{instruction}")
 
-# template for an instruction with input
-prompt_with_context = PromptTemplate(
-    input_variables=["instruction", "context"],
-    template="{instruction}\n\nInput:\n{context}")
+# # template for an instruction with input
+# prompt_with_context = PromptTemplate(
+#     input_variables=["instruction", "context"],
+#     template="{instruction}\n\nInput:\n{context}")
 
 hf_pipeline = HuggingFacePipeline(
     pipeline=InstructionTextGenerationPipeline(
@@ -136,6 +136,29 @@ hf_pipeline = HuggingFacePipeline(
 
 # llm_chain = LLMChain(llm=hf_pipeline, prompt=prompt)
 # llm_context_chain = LLMChain(llm=hf_pipeline, prompt=prompt_with_context)
+
+# COMMAND ----------
+
+from langchain.prompts.prompt import PromptTemplate
+
+_DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct Databricks SQL query to run, then look at the results of the query and return the answer.
+Use the following format:
+
+Question: "Question here"
+SQLQuery: "SQL Query to run"
+SQLResult: "Result of the SQLQuery"
+Answer: "Final answer here"
+
+Only use the table gnaf_vic_gold in the schema dannywong:
+
+{schema}.{table}
+
+If don't know the answer, just say you don't know.
+
+Question: {input}"""
+PROMPT = PromptTemplate(
+    input_variables=["input", "schema", "table"], template=_DEFAULT_TEMPLATE
+)
 
 # COMMAND ----------
 
@@ -155,8 +178,10 @@ engine = create_engine(
   connect_args={"http_path": endpoint_http_path}
 )
 
+
 db = SQLDatabase(engine, schema=None, include_tables=[table]) # schema=None to work around https://github.com/hwchase17/langchain/issues/2951 ?
-dolly_chain = SQLDatabaseChain(llm=hf_pipeline, database=db, verbose=True)
+dolly_chain = SQLDatabaseChain.from_llm(hf_pipeline, db, verbose=True, use_query_checker=True, return_intermediate_steps=True)
+#dolly_chain = SQLDatabaseChain(llm=hf_pipeline, database=db, verbose=True, use_query_checker=True, return_intermediate_steps=True)
 
 # COMMAND ----------
 
@@ -165,15 +190,23 @@ dolly_chain = SQLDatabaseChain(llm=hf_pipeline, database=db, verbose=True)
 
 # COMMAND ----------
 
-dolly_chain.run("What is the number of records?")
+result = dolly_chain("How many total records in the table?  The table name is gnaf_vic_gold.  Don't over complicate the query.")
+result
 
 # COMMAND ----------
 
-dolly_chain.run("How many records with the postcode 3128?")
+result = dolly_chain("How many records with the postcode 3128?  The table name is gnaf_vic_gold.  No JOINs, query single table.")
+result
 
 # COMMAND ----------
 
-dolly_chain.run("How many address label with the keyword 'GRANDVIEW RD'?  Can you list them all out?")
+result = dolly_chain("How many address label with the keyword 'GRANDVIEW RD'?  Can you list them all out?  The table name is gnaf_vic_gold")
+result
+
+# COMMAND ----------
+
+result = dolly_chain("How many records created after 2018?  The table name is gnaf_vic_gold.")
+result
 
 # COMMAND ----------
 
